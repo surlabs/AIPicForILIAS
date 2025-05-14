89dfd7ece4887cdfed47f8f08e8650fb69ae5522
Abraham <abrahammordev@gmail.com>
2025-05-14 14:27:29 +0200

    refactor: streamline image position and size functions for improved readability

--- templates/js/adjustImage.js
+++ templates/js/adjustImage.js
@@ -1,38 +1,36 @@
-// Configure the position of the image
-$('select[name="AIPicForm/input_6/input_10"]').on("change", function changePosition() {
-    const value = $(this).val();
+function changePosition() {
+    const value = $('select[name="AIPicForm/input_6/input_10"]').val();
 
-    // We have to apply the value to the style of the image
     const imgDiv = $('#imageDiv');
     imgDiv.removeClass("left center right");
     imgDiv.addClass(value);
-})
+}
 
-// Configure the size of the image
-$('input[name="AIPicForm/input_6/input_11"]').on("change", function changeSize() {
-    const value = parseInt($(this).val(), 10);
+function changeSize() {
+    const value = parseInt($('input[name="AIPicForm/input_6/input_11"]').val(), 10);
 
     const img = $('#imageDiv').children('img');
     img.removeClass('aipic-small aipic-medium aipic-large aipic-default-size');
 
     if (isNaN(value)) {
-
         img.addClass('aipic-default-size');
-
     } else {
-
         if (value <= 300) {
             img.addClass('aipic-small');
-
         } else if (value > 300 && value <= 500) {
             img.addClass('aipic-medium');
-
         } else if (value > 500) {
             img.addClass('aipic-large');
-
         } else {
             img.addClass('aipic-default-size');
         }
     }
-});
+}
 
+document.addEventListener("DOMContentLoaded", function() {
+    changePosition();
+    changeSize();
+
+    $('select[name="AIPicForm/input_6/input_10"]').on("change", changePosition);
+    $('input[name="AIPicForm/input_6/input_11"]').on("input", changeSize);
+});
